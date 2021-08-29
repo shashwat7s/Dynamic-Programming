917551f7b75a059fed4c81ac7e5a614eda1fc485
shashwat7s <shashwatsinghsikarwar@gmail.com>
2021-08-29 13:19:25 +0530

    dp impl for fib seq

--- fib.js
+++ fib.js
@@ -1,7 +1,21 @@
-const fib =(n) => {
- if(n<=2) return 1;
- return fib(n-1) + fib(n-2);
-};
+// Fibonacci Seq
+// Brute Force 
+// const fib =(n) => {
+//  if(n<=2) return 1;
+//  return fib(n-1) + fib(n-2);
+// };
+
+// memoization 
+// JS object, keys will be arg to func, value will be the return value
+
+const fib = (n, memo = {}) =>{
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    return memo[n];
+}
+
 
 console.log(fib(6))
 console.log(fib(7))
